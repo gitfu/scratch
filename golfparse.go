6b1a5d420be2e64f289f548238c64fd9d090d972
vis a vis <gitfu@users.noreply.github.com>
2017-01-26 17:48:45 -0500

    Update golfparse.go

--- golfparse.go
+++ golfparse.go
@@ -12,28 +12,28 @@ import (
 const sitemap string = "http://golfchannel.com/sitemap_video.xml"
 
 type UrlSet struct {
-// This represents <urlset>
+	// This represents <urlset>
 	VUrlList []Vurl `xml:"url"` //see Vurl struct
 }
 
 type Vurl struct {
-// This represents  a single <url> element, 
-// you can pick and choose which child elements to include	
+	// This represents  a single <url> element,
+	// you can pick and choose which child elements to include
 	Loc     string `xml:"loc"`
 	LastMod string `xml:"lastmod"`
 	Video   Vid    `xml:"video"` // see Vid struct
 }
 
 type Vid struct {
-// this represents a single <video> elememnt
-// Title , Content, and Desc are child node values	
+	// this represents a single <video> elememnt
+	// Title , Content, and Desc are child node values
 	Title   string `xml:"title"`
 	Content string `xml:"content_loc"`
 	Desc    string `xml:"description"`
 }
 
 func (v Vid) toXMLString() {
-	// Vid struct method to print struct values as xml 
+	// Vid struct method to print struct values as xml
 	bytes, err := xml.Marshal(v)
 	chk(err)
 	fmt.Printf("\n\n%s\n", string(bytes))
@@ -53,34 +53,21 @@ func chk(err error) {
 }
 
 func main() {
-	 // http get the xml file
 	res, err := http.Get(sitemap)
-	// check for errors
 	chk(err)
 	// keep file open so we can read it
 	defer res.Body.Close()
-	// read in the xml
 	xmlFile, err := ioutil.ReadAll(res.Body)
-	// check for errors
 	chk(err)
-	// declare a var of type UrlSet
 	var u UrlSet
 	// decode the xml into u, a UrlSet struct
 	xml.Unmarshal(xmlFile, &u)
-	// so we know how many videos we process
 	var count = 0
-	// go only does for loops
-	// u is a struct, that has an array of vurl structs
 	for _, vurl := range u.VUrlList {
-		// for each vurl struct in the array
-		// v is the vurl's Video struct
 		v := vurl.Video
-		// call the  Video struct's methods 
 		v.toXMLString()
 		v.show()
-		//print the count
-		fmt.Printf(" video # %d", count)
-		// increment count
+		fmt.Printf("video # %d", count)
 		count++
 	}
 }
